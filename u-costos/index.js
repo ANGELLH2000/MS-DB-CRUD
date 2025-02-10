15c463e9b43ef8c9612d1055ce7e23fe5fe3fb70
Angel Leon <angeleonh2000@gmail.com>
2025-02-10 15:00:22 -0500

    MS:u-costos

---
+++ u-costos/index.js
@@ -0,0 +1,97 @@
+import Chalk from 'chalk';
+import ora from 'ora';
+import { ConnectionMongoError, ConnectionRabbitMQError, ConnectionServerError, UpdateError } from './utils/error.js';
+import connectDB from './connections/mongo.js';
+import connectRabbitMQ from './connections/amqp.js';
+import config from './config/config.js';
+import startServer from './connections/server.js';
+import services from './services/services.js';
+import ApiErrors from './utils/apiErrors.js';
+
+/**
+ * Función principal que inicia el microservicio.
+ *
+ * Esta función realiza las siguientes tareas:
+ * 1. Conecta a MongoDB.
+ * 2. Conecta a RabbitMQ.
+ * 3. Inicia el servidor Express.
+ * 4. Escucha mensajes en la cola de RabbitMQ y los procesa.
+ *
+ * @async
+ * @function main
+ * @throws {ConnectionMongoError} Si ocurre un error al conectar a MongoDB.
+ * @throws {ConnectionRabbitMQError} Si ocurre un error al conectar a RabbitMQ.
+ * @throws {ConnectionServerError} Si ocurre un error al iniciar el servidor.
+ */
+async function main() {
+    console.log(Chalk.bold.bgGreen(" 🚀 Iniciando Microservicio: " + config.nameMicroservice + "  "));
+    const spinner = ora({ spinner: 'moon' });
+    const antennaSpinner = ora({
+        text: 'Esperando mensajes en la cola: ' + config.queue,
+        spinner: {
+            interval: 200, // Velocidad del cambio (ms)
+            frames: [
+                '📡 .',
+                '📡 ..',
+                '📡 ...',
+                '📡 ..',
+                '📡 .',
+            ]
+        }
+    });
+
+    try {
+
+        // Iniciar conexión con MongoDB,RabbitMQ y el Servidor
+        await connectDB();
+        const { channel } = await connectRabbitMQ();
+        await startServer();
+
+        // Escuchar mensajes en la cola de RabbitMQ y procesarlos con el servicio correspondiente 
+        antennaSpinner.start();
+        channel.consume(config.queue, async (message) => {
+            antennaSpinner.stopAndPersist({ symbol: '📩', text: `Mensaje recibido en la cola: ${config.queue}` });
+            try {
+                const { libreria, tokens, id_chat } = JSON.parse(message.content.toString());
+                const data = { libreria, tokens, id_chat };
+                const res = await services(data);
+                if (res) {
+                    const confirmation = { success: true, message: "Datos guardados correctamente" };
+                    channel.sendToQueue(message.properties.replyTo,
+                        Buffer.from(JSON.stringify(confirmation)),
+                        { correlationId: message.properties.correlationId }
+                    );
+                    console.log("✅ Datos guardados y confirmación enviada");
+                    channel.ack(message)
+                } else {
+                    channel.nack(message)
+                }
+
+            } catch (error) {
+                if (error instanceof UpdateError) {
+                    await ApiErrors(error);
+                    spinner.fail(error.message_error)
+                    const msg_confirmation = { success: false, message: error.message_error };
+                    channel.sendToQueue(message.properties.replyTo,
+                        Buffer.from(JSON.stringify(msg_confirmation)),
+                        { correlationId: message.properties.correlationId }
+                    );
+                }
+            }
+            console.log('\n')
+            antennaSpinner.start();
+
+        })
+
+    } catch (error) {
+        console.log("Existe un error")
+        await ApiErrors(error);
+        spinner.fail(error.message_error)
+        if (error instanceof ConnectionMongoError || error instanceof ConnectionRabbitMQError || error instanceof ConnectionServerError) {
+            console.log(Chalk.bold.bgRedBright(" 🚀💥 No se pudo iniciar el Microservicio: " + config.nameMicroservice + "  "));
+            process.exit(1);
+        }
+
+    }
+}
+main()
---
+++ u-costos/services/services.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { UpdateError } from "../utils/error.js";
+import ora from "ora";
+
+
+export default async function services(data) {
+    const { libreria, tokens, id_chat } = data;
+    if (!libreria || !tokens || !id_chat) {
+        let infoError= "Se encontró el sigiente campo vacío: " + (!libreria ? " [libreria] " : "" )+ (!tokens ? " [tokens] " : "") + (!id_chat ? " [id_chat] " : "")
+        throw new UpdateError(new Error(infoError), "Faltan datos para la actualización del documento.", data);
+    }
+    // Esquema de la colección
+    const chatSchema = new mongoose.Schema({
+        id_chat: mongoose.Schema.Types.ObjectId,
+        tokens: [{
+            model: { type: String, required: true },
+            input: { type: Number, required: true },
+            output: { type: Number, required: true }
+        }]
+    });
+
+    const Chat = mongoose.models[libreria] || mongoose.model(libreria, chatSchema, libreria);
+    const spinner = ora({ spinner: 'moon' });
+    try {
+        spinner.start("Actualizando documento...");
+        const res = await Chat.findOneAndUpdate(
+            { _id: id_chat },
+            { $push: { tokens: tokens } },
+            { new: true, upsert: false } // upsert: false, no crea un nuevo documento si no lo encuentra
+        );
+        if (!res) {
+            spinner.fail("No se encontró el documento con id_chat: " + id_chat);
+            throw new Error(`No se encontró el documento con id_chat: ${id_chat}.`);
+        }
+        spinner.succeed("Documento actualizado correctamente.");
+        return true;
+    } catch (error) {
+        spinner.stop();
+        throw new UpdateError(error, "Error en la actualización del documento.", data);
+    }
+}
