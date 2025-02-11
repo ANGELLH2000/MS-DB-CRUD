a4877a052ac372fbcdd56f7807e9a9633719189d
Angel Leon <angeleonh2000@gmail.com>
2025-02-11 10:29:35 -0500

    MS:r-conversacion

---
+++ r-conversacion/config/config.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+import { readFileSync } from 'fs';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+dotenv.config({ path: resolve(__dirname, '../.env') });
+
+// Package.json
+const packageJson = JSON.parse(readFileSync(resolve(__dirname, '../package.json')));
+
+/**
+ * Clase para gestionar la configuración de la aplicación.
+ *
+ * Esta clase carga las variables de entorno desde un archivo .env y las propiedades
+ * del archivo package.json para configurar las conexiones y otros parámetros de la aplicación.
+ *
+ * @class Configuracion,config
+ * @property {string} nameBD - Nombre de la base de datos.
+ * @property {string} mongoUri - URI de conexión a MongoDB.
+ * @property {string} amqpUrl - URL de conexión a RabbitMQ.
+ * @property {string} exchange - Nombre del intercambio en RabbitMQ.
+ * @property {string} queue - Nombre de la cola en RabbitMQ.
+ * @property {string} routingKey - Clave de enrutamiento en RabbitMQ.
+ * @property {number} max_cant_queue - Número máximo de mensajes en la cola.
+ * @property {number} port - Puerto en el que se ejecuta el servidor.
+ * @property {string} nameMicroservice - Nombre del microservicio.
+ * @property {string} hostApiErrors - URL del host para la API de errores.
+ */
+class Configuracion {
+    constructor() {
+        // Mongo connection
+        this.nameBD = "BD_Chat_conversacion",
+        this.mongoUri = process.env.MONGO_URI + this.nameBD,
+
+        // Amqp connection
+        this.amqpUrl = process.env.AMQP_URL,
+        this.exchange = "DB-CRUD",
+        this.queue = "R-CONVERSACION",
+        this.routingKey = "read.conversacion",
+        this.max_cant_queue = 10,
+
+        // Server connection
+        this.port = process.env.PUERTO || 8080,
+
+        // Microservice name
+        this.nameMicroservice = packageJson.name
+
+        //Host ApiErrors
+        this.hostApiErrors = process.env.HOST_API_ERRORS
+    }
+}
+const config = new Configuracion();
+export default config;
---
+++ r-conversacion/services/services.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { ReadError } from "../utils/error.js";
+import ora from "ora";
+
+
+export default async function services(data) {
+    const { libreria, id_chat } = data;
+    if (!libreria || !id_chat) {
+        let infoError = "Se encontró el sigiente campo vacío: " + (!libreria ? " [libreria] " : "") + (!id_chat ? " [id_chat] " : "")
+        throw new ReadError(new Error(infoError), "Faltan datos para la búsqueda del documento.", data);
+    }
+
+    // Esquema de la colección
+    const chatSchema = new mongoose.Schema({
+        chat: [{
+            emisor: { type: String, default:'' },
+            message: { type: String, default:'' }
+        }]
+    }, { timestamps: true });
+
+
+    const Chat = mongoose.models[libreria] || mongoose.model(libreria, chatSchema, libreria);
+    const spinner = ora({ spinner: 'moon' });
+    try {
+        spinner.start("Buscando nuevo documento...");
+        const res = await Chat.findById(id_chat).lean();
+        if (!res) {
+            spinner.fail(`No se pudo encontrar el documento con id: ${id_chat}.`);
+            throw new Error(`No se pudo encontrar el documento con id: ${id_chat}.`);
+        }
+        spinner.succeed("Documento encontrado con id: " + res._id);
+        const { _id, ...data } = res;
+        return {id_chat: _id, ...data};
+    } catch (error) {
+        spinner.stop();
+        throw new ReadError(error, `Error en la búsqueda del documento con id: ${id_chat}.`, data);
+    }
+}
